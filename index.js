362b6c7d259eb26153cab62d8648abaf7bc51ae7
ChitraChauhan <chitrasureshchauhan@gmail.com>
2025-11-06 12:40:28 +0530

    - Changes in index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/mean-ecommerce';
-mongoose.connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(MONGODB_URI)
     .then(() => console.log('MongoDB connected...'))
     .catch(err => console.log(err));
 
@@ -39,3 +39,4 @@ app.get('/', (req, res) => {
 // const PORT = process.env.PORT || 5000;
 
 // app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+module.exports = app;
